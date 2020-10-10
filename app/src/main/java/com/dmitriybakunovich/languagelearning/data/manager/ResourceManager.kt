fabea2a24a43a45c45a8bf2730ac090501e21a07
Dmitriy Bakunovich <dmitriy.bakunovich@gmail.com>
2020-10-10 17:25:32 +0300

    Different color when selecting text depending on device theme, updated dependencies

--- app/src/main/java/com/dmitriybakunovich/languagelearning/data/manager/ResourceManager.kt
+++ app/src/main/java/com/dmitriybakunovich/languagelearning/data/manager/ResourceManager.kt
@@ -1,19 +1,27 @@
 package com.dmitriybakunovich.languagelearning.data.manager
 
 import android.content.Context
+import android.content.res.Configuration
 import androidx.core.content.ContextCompat
 import com.dmitriybakunovich.languagelearning.R
 
 class ResourceManager(private val context: Context) {
-    fun getDisplayPixels(): Int =
-        (context.applicationContext.resources.displayMetrics.xdpi +
-                context.applicationContext.resources.displayMetrics.ydpi).toInt()
+    fun getDisplayPixels() = (context.applicationContext.resources.displayMetrics.xdpi +
+            context.applicationContext.resources.displayMetrics.ydpi).toInt()
     /*(context.applicationContext.resources.displayMetrics.heightPixels -
             context.applicationContext.resources.displayMetrics.widthPixels)*/
 
-    fun getMovingPixels(): Int = (context.applicationContext.resources.displayMetrics.xdpi /
+    fun getMovingPixels() = (context.applicationContext.resources.displayMetrics.xdpi /
             context.applicationContext.resources.displayMetrics.scaledDensity).toInt()
 
     fun getColorSelectText() =
-        ContextCompat.getColor(context.applicationContext, R.color.colorPrimary)
+        when (context.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+            Configuration.UI_MODE_NIGHT_NO -> ContextCompat.getColor(
+                context.applicationContext, R.color.colorAccentNight
+            )
+            Configuration.UI_MODE_NIGHT_YES -> ContextCompat.getColor(
+                context.applicationContext, R.color.colorPrimary
+            )
+            else -> ContextCompat.getColor(context.applicationContext, R.color.colorPrimary)
+        }
 }
